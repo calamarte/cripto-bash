d66e5da1b53cb7f78e3a5707446effe73fe319d0
calamarte <calamarte@gmail.com>
2022-03-28 14:01:27 +0200

    Added inspect operation

--- cripto-bash.sh
+++ cripto-bash.sh
@@ -19,13 +19,14 @@ trap ctrl_c INT
 function ctrl_c() {
     echo -e "\n${redColor}[!] Exit...\n${endColor}"
 
-    tput cnorm; exit 1
+    tput cnorm; rm -r $tmp; exit 1
 }
 
 lang="en"
 trans_all_url="https://www.blockchain.com/$lang/btc/unconfirmed-transactions"
 trans_url="https://www.blockchain.com/$lang/btc/tx"
 address_url="https://www.blockchain.com/$lang/btc/address"
+tmp=$(mktemp -d)
 
 function usage() {
     echo -e "\n${redColor}[!] Usage${endColor}"
@@ -39,44 +40,99 @@ function usage() {
     echo -e "\n\t${grayColor}[-h]${endColor}${yellowColor} Show this help panel"
     echo -e "\n"
 
-    tput cnorm; exit 1
+    tput cnorm; rm -r $tmp; exit 1
 }
 
 function get_unconfirmed() {
-    touch ut.tmp
+    touch $tmp/ut.tmp
 
-    while [ $(cat ut.tmp | wc -l) == 0 ]; do
+    while ! [[ -s $tmp/ut.tmp ]]; do
         curl -s $trans_all_url |
         html2text              |
-        sed -r '/^\s*$/d' > ut.tmp
+        sed -r '/^\s*$/d' > $tmp/ut.tmp
     done
 
     hashes=$(
-        cat ut.tmp           |
+        cat $tmp/ut.tmp      |
         grep "Hash" -A 2     |   
         grep -E -o '\[\w+\]' |   
         tr -d '[]'                 
     ) 
 
 
-    > ut.table
+    > $tmp/ut.table
     for hash in $hashes; do
-        block=$(grep $hash -A 6 ut.tmp)
-        echo $block | awk -v h=$hash '{print h" "$3" "$6" "$NF}' >> ut.table
+        block=$(grep $hash -A 6 $tmp/ut.tmp)
+        echo $block | awk -v h=$hash '{print h" "$3" "$6" "$NF}' >> $tmp/ut.table
     done
 
     total=$(
-        cat ut.table      |
+        cat $tmp/ut.table |
         awk '{print $NF}' |
         tr -d '$,'        |
         paste -s -d +     |
         bc
     )
 
-    echo $total | awk '{print "Total -- -- $"$1}' >> ut.table
+    echo $total | awk '{print "Total -- -- $"$1}' >> $tmp/ut.table
 
     echo -ne "${yellowColor}"
-    cat ut.table | column -o " | " -t -N 'Hash,Time,Amount(BTC),Amount($)' 
+    cat $tmp/ut.table | column -o " | " -t -N 'Hash,Time,Amount(BTC),Amount($)' 
+    echo -ne "${endColor}"
+}
+
+function inspect_transaction() {
+    touch $tmp/tr.tmp
+
+    while ! [[ -s $tmp/tr.tmp ]]; do
+        curl -s $trans_url/$1  |
+        html2text > $tmp/tr.tmp
+    done
+
+    fromToHashs=$(
+        grep -E 'From|To' -A 2 $tmp/tr.tmp |
+        grep -E -o '\[\w+\]'               |
+        tr -d '[]' 
+    )
+    
+    totals=$(
+        grep -E 'Total (Input|Output)' -A 2 $tmp/tr.tmp   |
+        grep "BTC" | tr ' ' '-'
+    )
+
+    outNumber=$(grep 'Outputs' -A 500 $tmp/tr.tmp | grep 'Address' | wc -l)
+    inNumber=$(grep 'Inputs' -A 500 $tmp/tr.tmp | grep 'Address' | echo "$(wc -l) - $outNumber" | bc)
+
+    grep 'Outputs' -A $((21 * $outNumber)) $tmp/tr.tmp |
+    grep 'Address' -A 6                                |
+    grep -E -o '(\[\w+\]|.*BTC$)'                      |
+    tr -d '[]'                                         |
+    sed 's/ BTC/-BTC/g'                                |
+    awk 'NR%2 {printf "%s ",$0;next;}1' > $tmp/tr-outputs.tmp
+    
+    grep 'Inputs' -A $((21 * $inNumber)) $tmp/tr.tmp     |
+    grep 'Address' -A 6                 |
+    grep -E -o '(\[\w+\]|.*BTC$)'       |
+    tr -d '[]'                          |
+    awk 'NR%2 {printf "%s ",$0;next;}1' |
+    sed 's/ BTC/-BTC/g' > $tmp/tr-inputs.tmp
+
+    echo "$fromToHashs $totals" | tr '\n' ' ' > $tmp/tr.table
+
+    echo -ne "${yellowColor}"
+    cat $tmp/tr.table | column -o " | " -t -N 'From,To,Total Input,Total Output' 
+    echo -ne "${endColor}"
+
+    echo -e '\n'
+    
+    echo -ne "${blueColor}"
+    cat $tmp/tr-inputs.tmp | column -o " | " -t -N 'Address,Amount (BTC)' 
+    echo -ne "${endColor}"
+    
+    echo -e '\n'
+    
+    echo -ne "${greenColor}"
+    cat $tmp/tr-outputs.tmp | column -o " | " -t -N 'Address,Amount (BTC)' 
     echo -ne "${endColor}"
 }
 
@@ -91,9 +147,11 @@ tput civis
 
 case $mode in
     unconfirmed) get_unconfirmed;;
-    inspect) echo $mode;;
+    inspect) inspect_transaction ${@:(-1)};;
     address) echo $mode;;
     *) usage;;
 esac
+
+rm -r $tmp
 
 tput cnorm
